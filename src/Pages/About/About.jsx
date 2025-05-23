0ed39bd27a5f70327fc638af0467afedf91776fb
Roshan-Silva <roshan0122silva@gmail.com>
2025-05-24 00:17:09 +0530

    Do the final changes

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import image_1 from '../../Assets/About/user1.jpg'
 import image_2 from '../../Assets/About/user2.jpg'
 import image_3 from '../../Assets/About/user3.jpg'
+import image_4 from '../../Assets/About/user4.png'
 const About = () => {
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -34,19 +35,29 @@ const About = () => {
         <h2>Meet Our Team</h2>
         <div className="team-members">
           <div className="team-member">
-            <img src={image_1} alt="Team Member 1" />
-            <p><strong>John Doe</strong></p>
-            <p>CEO & Founder</p>
+            <img src={image_4} alt="Team Member 1" />
+            <p><strong>Sachintha Roshan</strong></p>
+            <p>Team Member</p>
           </div>
           <div className="team-member">
-            <img src={image_2} alt="Team Member 2" />
-            <p><strong>Jane Smith</strong></p>
-            <p>Chief Operations Officer</p>
+            <img src={image_4} alt="Team Member 2" />
+            <p><strong>Thushara Navod</strong></p>
+            <p>Team Member</p>
           </div>
           <div className="team-member">
-            <img src={image_3} alt="Team Member 3" />
-            <p><strong>Michael Johnson</strong></p>
-            <p>Head of Education</p>
+            <img src={image_4} alt="Team Member 3" />
+            <p><strong>Asjad Ahmed</strong></p>
+            <p>Team Member</p>
+          </div>
+          <div className="team-member">
+            <img src={image_4} alt="Team Member 3" />
+            <p><strong>Fashan</strong></p>
+            <p>Team Member</p>
+          </div>
+          <div className="team-member">
+            <img src={image_4} alt="Team Member 3" />
+            <p><strong>Santhosh</strong></p>
+            <p>Team Member</p>
           </div>
         </div>
       </div>
